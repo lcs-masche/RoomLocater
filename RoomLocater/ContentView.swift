679c8b1d672fe7a651c1761eac60cfb7d7a3f6a3
Moritz Asche <masche@lcs.on.ca>
2022-03-25 11:26:57 -0400

    Initial Commit

---
+++ RoomLocater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoomLocater
+//
+//  Created by Moritz Asche on 2022-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoomLocater/RoomLocaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomLocaterApp.swift
+//  RoomLocater
+//
+//  Created by Moritz Asche on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct RoomLocaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
